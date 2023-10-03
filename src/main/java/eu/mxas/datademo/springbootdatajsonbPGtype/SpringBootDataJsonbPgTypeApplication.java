c9f38f40e69b276827f2c82bfcf73fb78a7a78cb
Mindaugas <mkarpinskas@exadel.com>
2023-10-03 12:54:11 +0300

    init 1st

---
+++ src/main/java/eu/mxas/datademo/springbootdatajsonbPGtype/SpringBootDataJsonbPgTypeApplication.java
@@ -0,0 +1,13 @@
+package eu.mxas.datademo.springbootdatajsonbPGtype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataJsonbPgTypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataJsonbPgTypeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/mxas/datademo/springbootdatajsonbPGtype/SpringBootDataJsonbPgTypeApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.mxas.datademo.springbootdatajsonbPGtype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDataJsonbPgTypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
